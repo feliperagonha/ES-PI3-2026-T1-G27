0b92219a9b2acfbc85bf39c31d07d8285bdaa24c
rafabrini <rafael.fg5@puccampinas.edu.br>
2026-05-13 20:52:58 -0300

    feat: private investor questions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,13 +2,13 @@ import { setGlobalOptions } from "firebase-functions/v2/options";
 
 setGlobalOptions({ maxInstances: 10 });
 
-// exporta todos os módulos
+//exporta todos os módulos
 export * from "./authentication";
 //export * from "./startups";
 export * from "./exchange";
 export * from "./wallet";
 //export * from "./transactions";
-//export * from "./questions";
+export * from "./questions";
 //export * from "./dashboard";
 //export * from "./notifications";
 //export * from "./users";
